9d441c0131202cdd92822ff2cc6d7cba4575d633
jeongminsu <ansuz0616@naver.com>
2022-02-15 12:35:18 +0900

    댓글 라우트

---
+++ routes/comment.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const Comments = require('../schemas/comments');
+const commentIdCount = require('../schemas/commentIdCount');
+
+const routes = express.Router();
+
+// 댓글 목록 조회
+routes.get('/posts/:post/comments', async (req, res) => {
+  const { post } = req.params;
+  const comment = await Comments.find({ postId: post });
+
+  res.status(200).json({ comment });
+})
+
+// 댓글 작성
+routes.post('/posts/:post/comments', async (req, res) => {
+  const { post } = req.params;
+  const { content, authorId } = req.body;
+
+
+  // 댓글 아이디 카운터
+  let id = await commentIdCount.find({ postId: Number(post) });
+  console.log(id);
+  if (!id.length) {
+    id = await commentIdCount.create({ postId: Number(post), num: 1 });
+  } else {
+    await commentIdCount.updateOne({ postId: Number(post), num: id[0].num }, { $set: { num: ++id[0].num } });
+    id = await commentIdCount.find({ postId: Number(post) });
+  }
+
+  const comment = await Comments.create({ postId: Number(post), commentId: id[0].num, content, authorId });
+
+  res.status(200).json({
+    commentId: id,
+    content,
+    authorId
+  });
+});
+
+// 댓글 수정
+routes.put('/posts/:post/:comment', async (req, res) => {
+  const { post, comment } = req.params;
+  const { content, authorId } = req.body;
+  let coment = await Comments.findOne({ postId: post, commentId: comment });
+  let boln = false;
+
+  if (coment.authorId === authorId) {
+    await Comments.updateOne({ postId: post, commentId: comment }, { $set: { content } });
+    boln = true;
+  }
+
+  res.status(200).json({
+    success: boln,
+    message: '수정 완료'
+  });
+});
+
+// 댓글 삭제
+routes.delete('/posts/:post/:comment/', async (req, res) => {
+  const { post, comment } = req.params;
+  const { authorId } = req.body;
+  const coment = Comments.findOne({ postId: post, commentId: comment });
+
+  if (coment.authorId === authorId) {
+    await Comments.deleteOne({ postId: post, commentId: comment });
+    res.status(200).json({
+      success: true,
+      message: '삭제 완료'
+    });
+  } else {
+    res.status(400).json({
+      success: false,
+      message: '삭제할 수 없는 댓글입니다.'
+    });
+  }
+
+});
+
+module.exports = routes;
