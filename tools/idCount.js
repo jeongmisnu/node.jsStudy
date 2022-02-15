ccafc90a0327616e6696a6c248086507128ec09c
jeongminsu <ansuz0616@naver.com>
2022-02-15 19:17:42 +0900

    모듈 수정

--- tools/idCount.js
+++ tools/idCount.js
@@ -8,49 +8,82 @@ const postIdCount = require('../schemas/postIdCount');
 const commentIdCount = require('../schemas/commentIdCount');
 
 // 지워진 ID 저장
-// async function RemoveId() {
-//   try {
-//     const id = await removeId.find({});
-//   } catch {
-//     console.log('실패');
-//     removeId.create({num: 0});
-//   }
-//   const id = await removeId.find({});
-//   let recyclingId = null;
+async function recyClingId(mode, num) {
+  let id = await RemoveId.find({mode});
+  let recyclingId = '';
 
-//   // 사용 안하는 아이디가 있다면 재사용
-//   if (id.length > 0) {
-//     recyclingId = id[0].num;
-//     removeId.deleteOne({ num: recyclingId });
-//   }
+  console.log(id[0].num);
 
-//   return recyclingId;
-// }
+  if (!id.length) {
+    console.log('ID: 없음');
+
+    recyclingId = null;
+  } else {
+    console.log('ID: 있음')
+
+    recyclingId = id[0].num;
+    RemoveId.deleteOne({ num: recyclingId });
+  }
+
+  return recyclingId;
+
+}
+
+exports.RemoveId = async (mode, num) => {
+  await RemoveId.create({ mode, num })
+
+  return 0;
+}
 
 // 게시글 ID 카운터
 exports.postCount = async () => {
   // let id = RemoveId();
-  let id = await postIdCount.find({});
+  let id = await recyClingId(1, 0);
 
-  // console.log(id);
-
-  if (!id.length) {
-    let newId = await postIdCount.create({num: 1});
-    // console.log(newId.num);
-    
-    return newId.num;
-
-  } else {
-    await postIdCount.updateOne({num: id[0].num}, {$set: {num: ++id[0].num}});
-
+  if (id === null) {
     id = await postIdCount.find({});
-    // console.log('보냄', id[0].num);
 
-    return id[0].num;
+    if (!id.length) {
+      await postIdCount.create({ num: 1 });
+      // console.log(newId.num);
+      let newId = await postIdCount.find({});
+
+      return newId[0].num;
+    } else {
+      await postIdCount.updateOne(
+        { num: id[0].num }, 
+        { $set: { num: ++id[0].num } }
+        );
+        
+      id = await postIdCount.find({});
+      // console.log('보냄', id[0].num);
+
+      return id[0].num;
+    }
+  } else {
+    return id;
   }
 }
 
 // 댓글 ID 카운터
-exports.commentCount = () => {
+exports.commentCount = async (postId) => {
+  let id = await commentIdCount.find({ postId: postId });
 
+  console.log(id);
+
+  if (!id.length) {
+    await commentIdCount.create({ postId: postId, num: 1 });
+    id = await commentIdCount.find({postId: postId});
+    console.log(id);
+    return id[0].num;
+  } else {
+    await commentIdCount.updateOne(
+      { postId: postId, num: id[0].num }, 
+      { $set: { num: ++id[0].num } }
+      );
+    
+    id = await commentIdCount.find({ postId: postId });
+
+    return id[0].num;
+  }
 }
